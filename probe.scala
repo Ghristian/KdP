639d6dc4bb7aeaa4a467ee9e3f2fd10e3b8e8c4b
Ghristian <44416068+Ghristian@users.noreply.github.com>
2023-12-11 22:12:23 +0100

    Add files via upload
    My first executed scala File

---
+++ probe.scala
@@ -0,0 +1,10 @@
+// Mein erstes Scala-Programm
+
+val name = "Bro" 
+
+@main
+def undLos = 
+	print("Pdvkdood#".map(x => (x.toInt-3).toChar))
+	println(name + ",")
+	println("Gx#kdvw#hv#mhw}w#jhvfkdiiw$".map(x => (x.toInt-3).toChar))
+	println("Vfdod#ixqnwlrqlhuw#qxq#hqgolfk$".map(x => (x.toInt-3).toChar))
