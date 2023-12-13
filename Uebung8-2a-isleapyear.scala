9049b7d9d57e4940b35a44295bf89b8ef89cf925
Ghristian <44416068+Ghristian@users.noreply.github.com>
2023-12-13 16:34:00 +0100

    Update Uebung8-2a-isleapyear.scala

--- Uebung8-2a-isleapyear.scala
+++ Uebung8-2a-isleapyear.scala
@@ -1,18 +1,16 @@
 // Checks if input is a leap year or not
-
-
+// Updated version
 import scala.io.StdIn
 
-
 @main
-def isleapy (n : Long) : String =
+def isleapy (x : Long) : Boolean =
 	print("Gebe ein Jahr > 0 ein: ")
 	val n : Long = StdIn.readLong()
 	if n % 400 == 0 then 
-			"True" 
+			true 
 	else if n % 4 == 0 && n % 100 == 0 then
-			"False"
+			false
 	else if n % 4 == 0 then
-			"True"
+			true
 	else
-			"False"
+			false
